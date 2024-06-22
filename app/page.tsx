7fefea2396164e1e825d3ffe98b37c7aba066369
Eduardo <eduardo.mantovani@ithinkupc.com>
2024-06-22 13:46:18 +0200

    vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="p-8">
+      <h1>Todo List</h1>
+      <form>
+      <input type="text" name="what" placeholder="Add a new todo" className="border-black p-1 mr-2 rounded" />
+      <button>Add Todo</button>
+      </form>
     </main>
   );
 }
