a81dea576a184ffcf9991e6f8ecfd5fe24709242
Eduardo <eduardo.mantovani@ithinkupc.com>
2024-06-23 17:05:19 +0200

    todo completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { actionAddTodo } from "@/actions/todos";
+import TodoForm from "@/components/TodoForm";
 import TodoItem from "@/components/TodoItem";
 import { dbGetTodos } from "@/lib/todos";
 
@@ -6,16 +7,12 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function Home() {
-
   const todos = await dbGetTodos();
 
   return (
     <main className="p-8">
       <h1>Todo List</h1>
-      <form action={actionAddTodo}>
-        <input type="text" name="what" placeholder="Add a new todo" className="border-black p-1 mr-2 rounded" />
-        <button>Add Todo</button>
-      </form>
+      <TodoForm />
       <ul>
         {todos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
